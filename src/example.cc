0a9bfbe3b7dfdaebad7604e0b7464ca833c708d2
eleonora.vangenechten <eleonora.vangenechten@gmail.com>
2019-03-09 13:26:27 +0100

    Rewrite example.cc

--- src/example.cc
+++ src/example.cc
@@ -2,31 +2,30 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+void print_vector(const std::vector<int>& vector)
+{
+  for (auto elem : vector) {
+    std::cout << elem << " ";
+  }
+  std::cout << '\n';
+}
+
 int main()
 {
   std::vector<int> test = { 4, 3, 6, 5, 1, 2 };
-  std::cout << "test start:"
-            << "\n";
-  for (auto elem : test) {
-    std::cout << elem << "\n";
-  }
   std::vector<int> second_vector = { 4, 3, 6, 5, 1, 2 };
-  auto dependent_vectors = { second_vector.begin() };
+  std::vector<int> third_vector = { 1, 2, 3, 4, 5, 6 };
+  auto dependent_vectors = { second_vector.begin(), third_vector.begin() };
   soa::SoaSort::sort<decltype(test.begin())>(test.begin(), test.end(),
       dependent_vectors);
-  std::cout << "test result:"
-            << "\n";
 
-  std::cout << "The array that determined the order:"
+  // Print the sorted test vector and the other 2 dependent_vectors.
+  std::cout << "first vector: "
             << "\n";
-  for (auto elem : test) {
-    std::cout << elem << "\n";
-  }
-
-  std::cout << "The second array:"
+  print_vector(test);
+  std::cout << "dependent_vectors: "
             << "\n";
-  for (auto elem : second_vector) {
-    std::cout << elem << "\n";
-  }
+  print_vector(second_vector);
+  print_vector(third_vector);
   return 0;
 }
