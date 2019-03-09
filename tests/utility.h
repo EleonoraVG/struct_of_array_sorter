d648d3268b7099bf0456fc4939467012115dd45d
Wouter De Keersmaecker <wouterdekrsmckr@gmail.com>
2019-03-09 18:38:07 +0100

    Added memory usage query function

---
+++ tests/utility.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <cstdint>
+#include <exception>
+
+uint64_t getMemoryUsage();
+
+
+#if defined(_WIN64) || defined(_WIN32)
+
+#include "windows.h"
+#include "psapi.h"
+
+uint64_t getMemoryUsage()
+{
+	PROCESS_MEMORY_COUNTERS_EX pmc;
+	if(GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc)) == 0)
+	{
+		throw std::exception("Failed to retrieve memory info");
+	}
+	return pmc.PrivateUsage;
+}
+
+#elif defined(__unix__)
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+
+int parseLine(char* line) {
+	// This assumes that a digit will be found and the line ends in " Kb".
+	int i = strlen(line);
+	const char* p = line;
+	while (*p <'0' || *p > '9') p++;
+	line[i - 3] = '\0';
+	i = atoi(p);
+	return i;
+}
+
+
+uint64_t getMemoryUsage()
+{
+	FILE* file = fopen("/proc/self/status", "r");
+	int result = -1;
+	char line[128];
+
+	while (fgets(line, 128, file) != NULL) {
+		if (strncmp(line, "VmSize:", 7) == 0) {
+			result = parseLine(line);
+			break;
+		}
+	}
+	fclose(file);
+
+	if(result == -1)
+	{
+		throw std::exception("Failed to retrieve memory info");
+	}
+
+	return result * 1024; //KB to bytes
+}
+
+#endif
+
